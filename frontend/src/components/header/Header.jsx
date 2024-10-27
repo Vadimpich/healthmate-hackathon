d56be1e26570969202eac87395dee30ca74e202c
Bogdan <btoporin@mail.ru>
2024-10-27 16:22:18 +0300

    animations

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -5,6 +5,12 @@ import logoImg from '../../assets/activity.svg';
 import './header.css';
 import {Link, useNavigate} from 'react-router-dom';
 import api from "../../api/index.js";
+import {motion} from 'framer-motion';
+import UserImg from '../../assets/Social Media Icons (Community)/user.svg'
+import MainImg from '../../assets/Social Media Icons (Community)/activity.svg'
+import LogOutImg from '../../assets/Social Media Icons (Community)/log-out.svg'
+
+
 
 function Header() {
     // Проверяем, есть ли токен доступа в localStorage
@@ -20,42 +26,108 @@ function Header() {
         navigate('/'); // Укажите нужный путь для перенаправления
     };
     return (
-        <Navbar expand="lg" className="bg-body-tertiary d-flex justify-content-between">
-            <Container fluid className="d-flex justify-content-between px-5">
-                <Navbar.Brand as={Link} to='/'>
-                    <div className="d-flex align-items-center">
-                        <img src={logoImg} alt='Logo'/>
-                        <text>HealthyMate</text>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{amount: 0.2, once: true}}
+        >
+            <Navbar expand="lg" className="bg-body-tertiary d-flex justify-content-between">
+                <Container fluid className="d-flex justify-content-between px-5">
+
+                    <Navbar.Brand as={Link} to='/'>
+
+                        <motion.div
+                            custom={1}
+                            variants={logoAnimation}
+                            className="d-flex align-items-center">
+                            <motion.img src={logoImg} alt='Logo'/>
+                            <text>HealthyMate</text>
+                        </motion.div>
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="navbarScroll"/>
+                    <div className="d-flex gap-2">
+                        {isLoggedIn ? (
+                            <>
+                                <motion.div
+                                    custom={1}
+                                    variants={buttonAnimation}
+                                >
+                                    <Button variant="success" className="custom-button d-flex align-items-center gap-1"
+                                            as={Link} to='/profile'>
+
+                                        <img src={UserImg} alt={'UserImg'}/>
+                                        Мои данные
+                                    </Button>
+                                </motion.div>
+                                <motion.div
+                                    custom={2}
+                                    variants={buttonAnimation}>
+                                    <Button variant="success" className="custom-button d-flex align-items-center gap-1"
+                                            as={Link} to='/main-page'>
+                                        <img src={MainImg} alt={"MainImg"}/>
+                                        Личный кабинет
+                                    </Button>
+                                </motion.div>
+                                <motion.div
+                                    custom={3}
+                                    variants={buttonAnimation}>
+                                    <Button variant="danger" onClick={handleLogout}
+                                            className="custom-button d-flex align-items-center gap-1">
+                                        <img src={LogOutImg} alt={"logOutImg"}/>
+                                        Выйти
+                                    </Button>
+                                </motion.div>
+
+
+                            </>
+                        ) : (
+                            <>
+                                <Button variant="success custom-button" as={Link} to='/login'>
+
+                                    Войти
+                                </Button>
+                                <Button variant="success" className="custom-button" as={Link} to='/registration'>
+                                    Регистрация
+                                </Button>
+                            </>
+                        )}
                     </div>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-                <div className="d-flex gap-2">
-                    {isLoggedIn ? (
-                        <>
-                            <Button variant="success" className="custom-button" as={Link} to='/profile'>
-                                Мои данные
-                            </Button>
-                            <Button variant="success" className="custom-button" as={Link} to='/main-page'>
-                                Личный кабинет
-                            </Button>
-                            <Button variant="danger" onClick={handleLogout} className="custom-button">
-                                Выйти
-                            </Button>
-                        </>
-                    ) : (
-                        <>
-                            <Button variant="success custom-button" as={Link} to='/login'>
-                                Войти
-                            </Button>
-                            <Button variant="success" className="custom-button" as={Link} to='/registration'>
-                                Регистрация
-                            </Button>
-                        </>
-                    )}
-                </div>
-            </Container>
-        </Navbar>
+                </Container>
+            </Navbar>
+        </motion.div>
     );
 }
 
 export default Header;
+
+export const logoAnimation = {
+    hidden: (custom) => ({
+        opacity: 0,
+        y: custom * 20,
+    }),
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.8,
+            ease: "easeInOut"
+        }
+    }
+};
+
+export const buttonAnimation = {
+    hidden: {
+        opacity: 0,
+
+    },
+    visible: (custom) => ({
+        opacity: 1,
+
+        transition: {
+            duration: 0.8,
+            ease: "easeInOut",
+            delay: custom * 0.2,
+        }
+    })
+};
--- frontend/src/components/promo/Promo.jsx
+++ frontend/src/components/promo/Promo.jsx
@@ -1,111 +1,209 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import DoctorImg from './../../assets/Врач.svg';
-import SportImag from './../../assets/Спорт.png'
-import FoodImg from '../../assets/Еда.png'
-import './promo.css'
+import SportImag from './../../assets/Спорт.png';
+import FoodImg from '../../assets/Еда.png';
+import './promo.css';
 import Button from "react-bootstrap/Button";
-import {Link} from "react-router-dom";
-import ClockImg from '../../assets/ClockPromo.svg'
+import { Link } from "react-router-dom";
+import ClockImg from '../../assets/ClockPromo.svg';
+import { motion } from 'framer-motion';
+
 
 function Promo() {
     return (
-        <>
-            <Container className="back">
-                <Row className="g-0 align-items-center">
-                    <Col md={5} className="d-flex flex-column justify-content-center">
-                        <div className="p-2">
-                            <h1 className="mb-4">Управляйте своим здоровьем с умом</h1>
-                            <p className="mb-4">
-                                Наше приложение для управления здоровьем и активностью — это ваш персональный помощник,
-                                который поможет оставаться в форме, поддерживать оптимальный режим сна и питания, а
-                                также
-                                следить за важными показателями здоровья!
-                            </p>
-                        </div>
-                        <div className='d-flex justify-content-center mb-4'>
-                            <Button as={Link} to='/registration' variant="success" className="custom-button promo">Присоединиться</Button>
-                        </div>
-                    </Col>
-                    <Col md={7} className="d-flex justify-content-center">
-                        <img
-                            src={DoctorImg} // Замените URL на свой
-                            alt="Пример"
-                            className="img-fluid" // Класс для адаптивного изображения
-                        />
-                    </Col>
-                </Row>
-            </Container>
-            <Container className="back bg-body">
-                <h2 className="fw-bold">Что умеет приложение?</h2>
-            </Container>
-            <Container className="back mb-4">
-                <Row className="g-0 align-items-center">
-                    <Col md={7} className="d-flex justify-content-center">
-                        <img
-                            src={SportImag} // Замените URL на свой
-                            alt="Пример"
-                            className="img-fluid" // Класс для адаптивного изображения
-                        />
-                    </Col>
-                    <Col md={5} className="d-flex flex-column justify-content-center">
-                        <div className="p-2">
-                            <h2 className="mb-4 fw-bold">Мониторинг активности</h2>
-                            <p className="mb-4 ">
-                                Отслеживайте шаги, пройденное расстояние, сожжённые калории и другие показатели, чтобы
-                                оставаться в движении и достигать целей!
-                            </p>
-                        </div>
+        <motion.div>
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ amount: 0.6, once: true }}
+                custom={0}
+                variants={boxAnimation}
+            >
+                <Container className="back">
+                    <Row className="g-0 align-items-center">
+                        <Col md={5} className="d-flex flex-column justify-content-center">
+                            <div className="p-2">
+                                <motion.h1
+                                    custom={1}
+                                    variants={logoAnimation}
+                                    className="mb-4"
+                                >
+                                    Управляйте своим здоровьем с умом
+                                </motion.h1>
+                                <motion.p
+                                    custom={2}
+                                    variants={logoAnimation}
+                                    className="mb-4"
+                                >
+                                    Наше приложение — это ваш персональный помощник,
+                                    который поможет оставаться в форме, а также следить за важными показателями здоровья!
+                                </motion.p>
+                            </div>
+                            <motion.div
+                                custom={3}
+                                variants={logoAnimation}
+                                className='d-flex justify-content-center mb-4'
+                            >
+                                <motion.div whileHover={{ scale: 1.2 }}>
+                                    <Button as={Link} to='/registration' variant="success"
+                                            className="custom-button promo">Присоединиться</Button>
+                                </motion.div>
+                            </motion.div>
+                        </Col>
+                        <Col md={7} className="d-flex justify-content-center">
+                            <motion.img
+                                custom={2}
+                                variants={logoAnimation}
+                                src={DoctorImg}
+                                alt="Пример"
+                                className="img-fluid"
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </motion.div>
 
-                    </Col>
+            {/* Second Section */}
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ amount: 0.6, once: true }}
+            >
+                <Container className="back bg-body">
+                    <motion.h2
+                        custom={3}
+                        variants={logoAnimation}
+                        className="fw-bold"
+                    >
+                        Что умеет приложение?
+                    </motion.h2>
+                </Container>
 
-                </Row>
-            </Container>
-            <Container className="back mb-4">
-                <Row className="g-0 align-items-center">
+                <motion.div custom={-1} variants={boxAnimation}>
+                    <Container className="back mb-4">
+                        <Row className="g-0 align-items-center">
+                            <Col md={7} className="d-flex justify-content-center">
+                                <motion.img
+                                    custom={2}
+                                    variants={logoAnimation}
+                                    src={SportImag}
+                                    alt="Пример"
+                                    className="img-fluid"
+                                />
+                            </Col>
+                            <Col md={5} className="d-flex flex-column justify-content-center">
+                                <div className="p-2">
+                                    <motion.h2 custom={1} variants={logoAnimation} className="mb-4 fw-bold">
+                                        Мониторинг активности
+                                    </motion.h2>
+                                    <motion.p custom={2} variants={logoAnimation} className="mb-4">
+                                        Отслеживайте шаги, пройденное расстояние, сожжённые калории и другие показатели, чтобы
+                                        оставаться в движении и достигать целей!
+                                    </motion.p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </motion.div>
+            </motion.div>
 
-                    <Col md={5} className="d-flex flex-column justify-content-center">
+            {/* Third Section */}
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ amount: 0.6, once: true }}
+                custom={1}
+                variants={boxAnimation}
+            >
+                <Container className="back mb-4">
+                    <Row className="g-0 align-items-center">
+                        <Col md={5} className="d-flex flex-column justify-content-center">
+                            <div className="p-2">
+                                <motion.h2 custom={1} variants={logoAnimation} className="mb-4 fw-bold">
+                                    Анализ сна
+                                </motion.h2>
+                                <motion.p custom={2} variants={logoAnimation} className="mb-4">
+                                    Получайте ценные рекомендации по улучшению качества сна на основе ежедневного анализа.
+                                </motion.p>
+                            </div>
+                        </Col>
+                        <Col md={7} className="d-flex justify-content-center">
+                            <motion.img
+                                custom={2}
+                                variants={logoAnimation}
+                                src={ClockImg}
+                                alt="Пример"
+                                className="img-fluid w-75"
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </motion.div>
 
-                        <div className="p-2">
-                            <h2 className="mb-4 fw-bold">Анализ сна</h2>
-                            <p className="mb-4 ">
-                                Получайте ценные рекомендации по улучшению качества сна на основе ежедневного анализа.
-                            </p>
-                        </div>
-
-                    </Col>
-                    <Col md={7} className="d-flex justify-content-center">
-                        <img
-                            src={ClockImg} // Замените URL на свой
-                            alt="Пример"
-                            className="img-fluid w-75" // Класс для адаптивного изображения
-                        />
-                    </Col>
-
-                </Row>
-            </Container>
-            <Container className="back mb-4">
-                <Row className="g-0 align-items-center">
-                    <Col md={7} className="d-flex justify-content-center">
-                        <img
-                            src={FoodImg} // Замените URL на свой
-                            alt="Пример"
-                            className="img-fluid" // Класс для адаптивного изображения
-                        />
-                    </Col>
-                    <Col md={5} className="d-flex flex-column justify-content-center">
-                        <div className="p-2">
-                            <h2 className="mb-4 fw-bold">Питание и калории</h2>
-                            <p className="mb-4">
-                                Не забывайте пить воду и следить за питанием с нашими напоминаниями и удобным дневником.
-                            </p>
-                        </div>
-
-                    </Col>
-
-                </Row>
-            </Container>
-        </>
+            {/* Fourth Section */}
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ amount: 0.6, once: true }}
+                custom={-1}
+                variants={boxAnimation}
+            >
+                <Container className="back mb-4">
+                    <Row className="g-0 align-items-center">
+                        <Col md={7} className="d-flex justify-content-center">
+                            <motion.img
+                                custom={2}
+                                variants={logoAnimation}
+                                src={FoodImg}
+                                alt="Пример"
+                                className="img-fluid"
+                            />
+                        </Col>
+                        <Col md={5} className="d-flex flex-column justify-content-center">
+                            <div className="p-2">
+                                <motion.h2 custom={1} variants={logoAnimation} className="mb-4 fw-bold">
+                                    Питание и калории
+                                </motion.h2>
+                                <motion.p custom={2} variants={logoAnimation} className="mb-4">
+                                    Не забывайте пить воду и следить за питанием с нашими напоминаниями и удобным дневником.
+                                </motion.p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </motion.div>
+        </motion.div>
     );
 }
 
 export default Promo;
+
+export const boxAnimation = {
+    hidden: (custom) => ({
+        opacity: 0,
+        x: custom < 0 ? -50 : 50,
+    }),
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.8,
+            ease: "easeInOut"
+        }
+    }
+};
+
+export const logoAnimation = {
+    hidden: (custom) => ({
+        opacity: 0,
+        y: custom * 20,
+    }),
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.8,
+            ease: "easeInOut"
+        }
+    }
+};
